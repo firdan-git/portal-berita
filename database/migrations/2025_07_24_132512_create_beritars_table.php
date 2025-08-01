e1916eb712c140ed3c9243484eb9317e94a364b3
firdan-git <whyya9@gmail.com>
2025-08-01 21:32:03 +0700

    first commit

---
+++ database/migrations/2025_07_24_132512_create_beritars_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('beritars', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug')->unique();
+            $table->text('isi');
+            $table->string('gambar')->nullable();
+            $table->foreignId('kategori_id')->constrained('kategoris');
+            $table->foreignId('user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beritars');
+    }
+};
